d12289c7961cd5afeb45d936974e6818ae516ef1
shreyasshivakumara <shreyas004@gmail.com>
2021-02-15 13:40:52 +0100

    http feature practise

--- node_projects/index.js
+++ node_projects/index.js
@@ -1,5 +1,17 @@
 const Person = require('./person.js');
 const Logger = require('./logger');
+const http = require('http');
+
+
+const server = http.Server((req,res)=>{
+    if (req.url === '/'){
+        res.end("<h1>HOME SWEET HOME</h1>");
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT,() => {console.log(`Server Running!! in ${this.PORT}`)});
 
 const person1 = new Person('Shreya Shivakumara', 30,9,6);
 
@@ -7,6 +19,6 @@ const person1 = new Person('Shreya Shivakumara', 30,9,6);
 
 const logger = new Logger();
 
-logger.on('message', (data) => console.log(`Called listener :`, data));
+//logger.on('message', (data) => console.log(`Called listener :`, data));
 
-logger.log('Hey!');
+//logger.log('Hey!');
